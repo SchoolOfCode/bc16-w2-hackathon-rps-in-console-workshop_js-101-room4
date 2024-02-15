ff2eae0541222bf7f81ab9a6f9b054259d873094
hiron4eva <hiron4eva@gmail.com>
2024-02-15 13:51:50 +0000

    task 3

--- script.js
+++ script.js
@@ -23,3 +23,35 @@ function makeMove(){
 }
 
 makeMove();
+
+let userWins = false;
+let computerWins = false;
+let draw = false;
+
+
+function getScore(playerMove, computerMove){
+    if (
+        (userMove=="rock" && computerMove=="scissors") ||
+        (userMove=="scissors" && computerMove=="paper") ||
+        (userMove=="paper" && computerMove=="rock") 
+    ) {
+        console.log("user wins");
+        userWins = true;
+    }
+    else if (
+        (userMove=="rock" && computerMove=="paper") ||
+        (userMove=="scissors" && computerMove=="rock") ||
+        (userMove=="paper" && computerMove=="scissors") 
+    ) {
+        console.log("computer wins");
+        computerWins = true;
+    }
+    else (
+        (userMove=="rock" && computerMove=="rock") ||
+        (userMove=="scissors" && computerMove=="scissors") ||
+        (userMove=="paper" && computerMove=="paper") 
+    ) {
+        console.log("its a draw");
+        draw = true;
+    }
+}
